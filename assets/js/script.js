efc3815bd43948739104042dfdacc3df3e47c4c0
Gunel99 <45911420+Gunel99@users.noreply.github.com>
2022-05-07 13:11:42 +0400

    Added default structure

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $(".multiple-items").slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    speed: 7000,
+    delay: 3000,
+    dots: true,
+  });
+});
+
+// Navbar Click start
+$(".navbar_click_icon .icon .openSidebar").click(function() {
+  document.body.classList.add("active");
+});
+
+$("header .sidebar .close").click(function() {
+  document.body.classList.remove("active");
+});
